00b8edf8117c81e21062d88a1be4d4604fefa4ff
zhuang002 <zhuang001@gmail.com>
2020-08-08 14:48:13 -0400

    init

---
+++ src/minimumpath/MinimumPath.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimumpath;
+
+/**
+ *
+ * @author zhuan
+ */
+public class MinimumPath {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int[][] map=null;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        inputGraph();
+        
+    }
+
+    private static void inputGraph() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
